b711f79f522c6c27093ad45fae0a2c2a575b019d
Suttirak Mattayawerakiat <6210110383@psu.ac.th>
2022-03-11 20:20:02 +0700

    + frontend : image, like & dislike button + backend: simple api to get data + add some dependencies

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,6 +1,48 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const axios = require('axios')
+const cors = require('cors')
+
 const app = express()
+app.use(cors())
 const port = 5000
+
+const only_cat_data = {
+    'mabin_canny_stage_09' : {
+        reaction: {
+            like: 0,
+            dislike: 0
+        }
+    }
+}
+
+app.get('/api/data/reactions', (req, res) => {
+    let img = req.query.img
+    // image not found
+    if(!only_cat_data[img])
+    {
+        res.sendStatus(404)
+        return
+    }
+    // send json of reaction of the target image
+    res.send(JSON.stringify(only_cat_data[img].reaction))
+    console.log(only_cat_data[img].reaction)
+})
+
+app.post('/api/react', bodyParser.json(), (req, res) => {
+    let img = req.body.img
+    let reaction = req.body.reaction
+    // image not found
+    if(!only_cat_data[img])
+    {
+        res.sendStatus(404)
+        return
+    }
+    only_cat_data[img].reaction.like += reaction.like
+    only_cat_data[img].reaction.dislike += reaction.dislike
+    console.log(only_cat_data)
+    res.sendStatus(200)
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import mabin from './assets/img/mabin_canny_09.png'
+
+const axios = require('axios')
 
 function App() {
+
+  const [reaction, setReaction] = useState({ like: 0, dislike: 0 })
+
+  const onBtnLike_click = (e) => {
+    const data = {img: 'mabin_canny_stage_09', reaction: {like: 1, dislike: 0}}
+    axios.post('http://localhost:5000/api/react', data)
+      .then((res) => {
+        updateData()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const onBtnDislike_click = (e) => {
+    const data = {img: 'mabin_canny_stage_09', reaction: {like: 0, dislike: 1}}
+    axios.post('http://localhost:5000/api/react', data)
+      .then((res) => {
+        updateData()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const updateData = () => {
+    axios.get('http://localhost:5000/api/data/reactions?img=mabin_canny_stage_09')
+      .then((res) => {
+        setReaction(res.data)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
+  useEffect(() => {
+    updateData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={mabin} alt="" />
+      <div>
+        <button onClick={onBtnLike_click}>Like: {reaction.like}</button>
+        <button onClick={onBtnDislike_click}>Dislike: {reaction.dislike}</button>
+      </div>
     </div>
   );
 }
