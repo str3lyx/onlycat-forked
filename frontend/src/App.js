46c6174df0cd699d5c61454d4fdfee4806324047
Piravit Chenpittaya <karnzx-18762@hotmail.com>
2022-03-13 17:59:29 +0700

    add mui, sweetalert2, menuappbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import mabin from './assets/img/mabin_canny_09.png'
 import ImagePost from './ImagePost';
+import MenuAppBar from './components/appbar'
 
 function App() {
 
   return (
     <div>
-      <ImagePost img="mabin_canny_stage_09" url={mabin}/>
+      <MenuAppBar />
+      <ImagePost img="mabin_canny_stage_09" url={mabin} />
     </div>
   );
 }
