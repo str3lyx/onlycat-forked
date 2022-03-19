4e0524a1b450c5e21c5aa973d4c5ce25780d5d4a
Piravit Chenpittaya <karnzx-18762@hotmail.com>
2022-03-19 22:12:11 +0700

    add winston, express winston & feat log express route

--- backend/src/main.js
+++ backend/src/main.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser')
 const axios = require('axios')
 const cors = require('cors')
 const jwt = require('jsonwebtoken')
+const winston = require('winston');
+const expressWinston = require('express-winston');
+
 // this secret only use for development use(require('crypto').randomBytes(64).toString('hex'))
 const TOKEN_SECRET = 'this-is-my-secret-for-development'
 const app = express()
@@ -10,6 +13,25 @@ const port = process.env['SERVER_PORT'] || 5000
 
 app.use(cors())
 app.use('/img', express.static('public'))
+app.use(expressWinston.logger({ // level info by default
+    transports: [
+        new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple(),
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    ),
+    // format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+    meta: false,
+    msg: "Express {{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}",
+    // expressFormat: true, // override all msg, if true above config "msg" wont work
+    colorize: true,
+    ignoreRoute: function (req, res) { return false; }
+}));
 
 const only_cat_data = {
     'mabin_canny_stage_09': {
@@ -52,7 +74,7 @@ const authenticated = (req, res, next) => {
 }
 
 app.get('/api/data', (req, res) => {
-    if(Object.keys(only_cat_data).find(data => data === req.query.img))
+    if (Object.keys(only_cat_data).find(data => data === req.query.img))
         res.send(JSON.stringify(only_cat_data[req.query.img]))
     else
         res.send(JSON.stringify(Object.keys(only_cat_data)))
