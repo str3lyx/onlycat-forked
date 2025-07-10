bae4b90ec46bf236f0795778ee6e4aa94b1a6b94
str3lyx <raksuttirak2@gmail.com>
2025-07-11 00:28:59 +0700

    backend: add methods associated with post_save

--- onlycat-backend/frameworks/base/models/__init__.py
+++ onlycat-backend/frameworks/base/models/__init__.py
@@ -1,11 +1,14 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import signals
+from django.dispatch import receiver
 from django.utils import timezone
 from django_currentuser.db.models import CurrentUserField
+from typing import Self
 from uuid import uuid4
 
 
-class AbstractModelQuerySet(models.QuerySet):
+class AbstractModelQuerySet[T: models.Model](models.QuerySet[T]):
     def actives(self):
         return self.filter(is_active=True)
     
@@ -13,7 +16,7 @@ class AbstractModelQuerySet(models.QuerySet):
         return self.filter(is_active=False)
 
 
-class AbstractModelManager(models.Manager):
+class AbstractModelManager[T: models.Model](models.Manager[T]):
     def get_queryset(self):
         return AbstractModelQuerySet(self.model, using=self._db)
     
@@ -29,11 +32,17 @@ class AbstractModel(models.Model):
     is_active = models.BooleanField(editable=False, default=True)
     created_at = models.DateTimeField(default=timezone.now)
     created_by = CurrentUserField(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='+')
-    objects: AbstractModelManager = AbstractModelManager()
+    objects: AbstractModelManager[Self] = AbstractModelManager()
 
     @staticmethod
     def base_attrs():
         return ['id', 'is_active', 'created_at', 'created_by']
+    
+    def on_created(self):
+        pass
+
+    def on_updated(self):
+        pass
 
     def delete(self, *args, **kwargs):
         self.is_active = False
@@ -42,3 +51,14 @@ class AbstractModel(models.Model):
     class Meta:
         abstract = True
         ordering = ['-created_at']
+
+
+@receiver(signals.post_save)
+def post_save_callback(sender, instance, created, raw, using, update_fields, **kwargs):
+    if not instance or hasattr(instance, '_dirty'):
+        return
+    
+    instance._dirty = True
+    func = 'on_created' if created else 'on_update'
+    getattr(sender, func, lambda *_: None)(instance)
+    del instance._dirty
